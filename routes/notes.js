d882e8ff356d4585ef389ed0cccf6949450a1aa9
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-10-27 22:15:30 -0400

    added notes.js code including notes.get and notes.post functions

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,23 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helper/fsUtils');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note successfully added!');
+    } else {
+        res.errored('There was an error in adding note')
+    }
+});
+
+module.exports = notes;
